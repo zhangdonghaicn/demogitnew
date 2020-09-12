104073bfca36fc44d71028d5fc6b72a85f80d31a
zhangdonghaicn <2673290505@qq.com>
2020-09-13 04:19:15 +0800

    Create Test.java

---
+++ demogit/src/main/java/com/itdonghai/Test.java
@@ -0,0 +1,14 @@
+package com.itdonghai;
+
+/**
+ * @author zzddhh
+ * @date 2020/9/13
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("创建test 分支");
+        System.out.println("创建test 分支");
+        System.out.println("在线更新");
+    }
+}
