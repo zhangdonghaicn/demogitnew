2911ce15f6d6dd3b67b4398ac9d0595d3e146971
zhangdonghai <zhangdonghai@htaiot.com>
2020-09-13 03:11:32 +0800

    更新1

--- demogit/src/main/java/com/itdonghai/HelloGit.java
+++ demogit/src/main/java/com/itdonghai/HelloGit.java
@@ -9,5 +9,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("更新1");
+        System.out.println("更新2");
     }
 }
